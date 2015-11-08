431aa52171675a8cc0df2fd8bc923df47862d4f6
La Musique <lamusique@users.noreply.github.com>
2015-11-09 00:17:09 +0900

    Put more boxes in the display.

--- src/main/scala/RotatePushPop.scala
+++ src/main/scala/RotatePushPop.scala
@@ -1,15 +1,6 @@
 import processing.core.PApplet
 import processing.core.PConstants
 
-import java.util.HashMap; 
-import java.util.ArrayList; 
-import java.io.File; 
-import java.io.BufferedReader; 
-import java.io.PrintWriter; 
-import java.io.InputStream; 
-import java.io.OutputStream; 
-import java.io.IOException; 
-
 class RotatePushPop extends PApplet {
 
   /**
@@ -20,13 +11,16 @@ class RotatePushPop extends PApplet {
    * and pop() restores the prior coordinate system.
    */
 
-  var a: Float = 0.0f;
   // Angle of rotation
-  var offset: Float = PConstants.PI / 24f
+  var angle: Float = 0.0f
+
   // Angle offset between boxes
-  var num: Int = 12
+  // As this denominator increases, slices are finer.
+  var offset: Float = PConstants.PI / 96f
 
   // Number of boxes
+  val BOXES: Int = 120
+
 
   override def setup() {
     noStroke()
@@ -34,34 +28,35 @@ class RotatePushPop extends PApplet {
 
   override def draw() {
 
-    lights();
+    lights()
+    background(0, 0, 26)
+    translate(width / 2, height / 2)
 
-    background(0, 0, 26);
-    translate(width / 2, height / 2);
+    val seq = 0 to BOXES
 
-    val seq = 0 to num
-
+    // Paint process needs to be serial.
     seq.foreach(i => {
-      val gray = PApplet.map(i, 0, num - 1, 0, 255);
-      pushMatrix();
-      fill(gray);
-      rotateY(a + offset * i);
-      rotateX(a / 2 + offset * i);
+      val gray = PApplet.map(i, 0, BOXES - 1, 0, 255)
+      pushMatrix()
+      fill(gray)
+      rotateY(angle + offset * i)
+      rotateX(angle / 2 + offset * i)
       box(200);
       popMatrix();
     })
 
-    a += 0.01f;
+    angle += 0.01f
   }
 
-  //override def settings() {  size(640, 360, PConstants.P3D); }
   override def settings() {
     size(640, 360, PConstants.P3D);
   }
 }
+
 object RotatePushPop {
   def main(args: Array[String]) {
 
+    // This specifies the class to be instantiated.
     val appletArgs = Array("RotatePushPop")
     if (args != null) {
       PApplet.main(PApplet.concat(appletArgs, args));
