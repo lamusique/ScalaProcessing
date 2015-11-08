d00e9d1ae1c6db93f1d1fbd4ddb399a10e06123d
La Musique <lamusique@users.noreply.github.com>
2015-11-08 23:51:04 +0900

    Port Java to Scala totally.

--- src/main/scala/RotatePushPop.scala
+++ src/main/scala/RotatePushPop.scala
@@ -10,57 +10,59 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
-object RotatePushPop extends PApplet {
+class RotatePushPop extends PApplet {
 
-/**
- * Rotate Push Pop. 
- * 
- * The push() and pop() functions allow for more control over transformations.
- * The push function saves the current coordinate system to the stack 
- * and pop() restores the prior coordinate system. 
- */
- 
-var a:Float = 0.0f;               // Angle of rotation
-var offset:Float = PConstants.PI/24.0f;  // Angle offset between boxes
-var num:Int = 12;            // Number of boxes
+  /**
+   * Rotate Push Pop.
+   *
+   * The push() and pop() functions allow for more control over transformations.
+   * The push function saves the current coordinate system to the stack
+   * and pop() restores the prior coordinate system.
+   */
 
-override def setup() {
-  noStroke()
-}
+  var a: Float = 0.0f;
+  // Angle of rotation
+  var offset: Float = PConstants.PI / 24f
+  // Angle offset between boxes
+  var num: Int = 12
 
-override def draw() {
+  // Number of boxes
 
-  lights();
-  
-  background(0, 0, 26);
-  translate(width/2, height/2); 
-  
-  for (i <- 0 to num) {
-    val gray = PApplet.map(i, 0, num-1, 0, 255);
-    pushMatrix();
-    fill(gray);
-    rotateY(a + offset*i);
-    rotateX(a/2 + offset*i);
-    box(200);
-    popMatrix();
+  override def setup() {
+    noStroke()
   }
-  
-  a += 0.01f;    
-} 
+
+  override def draw() {
+
+    lights();
+
+    background(0, 0, 26);
+    translate(width / 2, height / 2);
+
+    val seq = 0 to num
+
+    seq.foreach(i => {
+      val gray = PApplet.map(i, 0, num - 1, 0, 255);
+      pushMatrix();
+      fill(gray);
+      rotateY(a + offset * i);
+      rotateX(a / 2 + offset * i);
+      box(200);
+      popMatrix();
+    })
+
+    a += 0.01f;
+  }
 
   //override def settings() {  size(640, 360, PConstants.P3D); }
-  override def settings() {  size(640, 360, PConstants.P3D); }
-
+  override def settings() {
+    size(640, 360, PConstants.P3D);
+  }
+}
+object RotatePushPop {
   def main(args: Array[String]) {
 
-    val libPath = System.getProperty("java.library.path")
-    println(libPath)
-    val userDir = System.getProperty("user.home")
-    println(userDir)
-    System.setProperty("java.library.path", libPath +";"+"\\.ivy2\\cache\\org.jogamp.gluegen\\gluegen-rt\\jars")
-    //"C:\\Users\\neko\\.ivy2\\cache\\org.jogamp.gluegen\\gluegen-rt\\jars\\gluegen-rt-2.3.2-natives-windows-i586.jar"
-
-    val appletArgs = Array("JavaRotatePushPop")
+    val appletArgs = Array("RotatePushPop")
     if (args != null) {
       PApplet.main(PApplet.concat(appletArgs, args));
     } else {
