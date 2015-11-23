4ef27f1387c161a8fcae7aebc8755e340477c06c
La Musique <lamusique@users.noreply.github.com>
2015-11-24 00:30:48 +0900

    Macros Explosions

--- src/main/scala/com/nekopiano/scala/processing/ScalaPVector.scala
+++ src/main/scala/com/nekopiano/scala/processing/ScalaPVector.scala
@@ -7,7 +7,7 @@ import processing.core.PVector
  * Created by neko on 2015/11/23.
  */
 //class ScalaPVector(val sx:Float, val sy:Float, val sz:Float) extends PVector {
-case class ScalaPVector(var x:Float, var y:Float, var z:Float) {
+case class ScalaPVector(var x:Float, var y:Float, var z:Float = 0) {
   private val pVector = new PVector(x, y, z)
 
   def angleWith(vector: ScalaPVector):Float = PVector.angleBetween(this.pVector, vector.pVector)
