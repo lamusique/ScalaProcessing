b4ca7ad8c1041a7e601777b18dd75e82f3a76a7c
La Musique <lamusique@users.noreply.github.com>
2015-12-07 00:13:24 +0900

    Improve camera view app - operate the mouse line angles with arrow keys - display points

--- src/main/scala/com/nekopiano/scala/processing/sandbox/CameraViewApp.scala
+++ src/main/scala/com/nekopiano/scala/processing/sandbox/CameraViewApp.scala
@@ -1,11 +1,17 @@
 package com.nekopiano.scala.processing.sandbox
 
-import com.nekopiano.scala.processing.{ScalaPVector, ScalaPApplet}
+import com.nekopiano.scala.processing.{Camera, Angles, ScalaPVector, ScalaPApplet}
 
 /**
  * Created by neko on 2015/11/29.
  */
 class CameraViewApp extends ScalaPApplet {
+
+  import com.nekopiano.scala.processing.Angles._
+
+  implicit val sp5 = this
+
+  val cameraView = new Camera
 
   override def settings: Unit = {
     size(1024, 768, P3D)
@@ -13,16 +19,13 @@ class CameraViewApp extends ScalaPApplet {
     // size(width, height, P3D)
   }
   override def setup: Unit = {
+    cameraView.initialize()
   }
   override def draw: Unit = {
-    background(225)
+    background(180)
 
     //camera(width/2.0f, height/2.0f, (height/2.0f) / tan((PI/3f) / 2.0f), width/2.0f, height/2.0f, 0, 0, 1, 0)
     //camera(width/2.0f, height/2.0f, mouseY, width/2.0f, height/2.0f, 0, 0, 1, 0)
-
-    val eye = ScalaPVector(width/2.0f,height/2.0f,0)
-    val center = ScalaPVector(width/2.0f,height/2.0f,-500)
-    //camera(eye, center)
 
     // init perspective projection based on new dimensions
     val cameraFOV = 60 * DEG_TO_RAD
@@ -33,15 +36,28 @@ class CameraViewApp extends ScalaPApplet {
     val cameraFar = cameraZ * 10.0f
     val cameraAspect = width.toFloat / height.toFloat
 
+    // by default
+    //val eye = ScalaPVector(width/2.0f, height/2.0f, cameraZ)
+    //val center = ScalaPVector(width/2.0f, height/2.0f, 0)
+
+    //val eye = ScalaPVector(width/2.0f, height/2.0f, 0)
+    //val center = ScalaPVector(width/2.0f, height/2.0f, 0)
+
+    //camera(eye, center)
+
+    //camera(ScalaPVector(200, 100, 400), center)
     //perspective(PI/3f, width/height, cameraNear, cameraFar)
 
+
+    cameraView.camera()
+
     usingMatrix {
-      translate(eye)
+      translate(cameraView.eye)
       point(ScalaPVector.origin)
       text("eye", ScalaPVector.origin)
     }
     usingMatrix {
-      translate(center)
+      translate(cameraView.center)
       point(ScalaPVector.origin)
       text("center", ScalaPVector.origin)
     }
@@ -51,19 +67,69 @@ class CameraViewApp extends ScalaPApplet {
       box(5)
     }
 
-    usingMatrix {
+    val mousePoints = usingMatrix {
       val mouse = getMouseVector()
-      val rearMouse = mouse.setZ(-8000)
-      val angleY = (PI/3/2) * (mouseY - height/2f)/(height/2f)
+      //val rearMouse = mouse.setZ(-8000)
+
+      // make the mouse position the rotating center
+      translate(mouse)
+
+      applyKeyPressedAngles()
+
+      val baseAngle = PI/3
+      //val angleY = baseAngle * (mouseY - height/2f)/(height/2f)
       //rotateY(PI/3/2)
-      rotateX(angleY)
-      val angleX = (PI/3/2) * (width/2f - mouseX)/(width/2f)
-      rotateY(angleX)
-      text("angleX="+angleX,mouse)
-      line(mouse, rearMouse)
+      rotateX(angles.y)
+      //text("angleY="+ degrees(angleY) + "°", mouse.addY(20))
+      text("angleY="+ angles.yDegrees + "°", ScalaPVector.origin.addY(20))
+
+      //val angleX = baseAngle * (width/2f - mouseX)/(width/2f)
+      rotateY(angles.x)
+      //text("angleX="+ degrees(angleX) + "°", mouse.addY(-20))
+      text("angleX="+ angles.xDegrees + "°", ScalaPVector.origin.addY(-20))
+
+      //line(mouse, rearMouse)
+      line(ScalaPVector.origin, ScalaPVector.origin.setZ(-10000))
+      val mouseRearOnScreen = screen(ScalaPVector.origin.setZ(-10000))
+      val mouseRearOnModel = model(ScalaPVector.origin.setZ(-10000))
+      (mouseRearOnScreen,mouseRearOnModel)
+    }
+
+
+    val mouse = getMouseVector()
+    text("mouse=" + mouse, width/3, height - 95)
+    text("mouseRearOnScreen=" + mousePoints._1, width/3, height - 80)
+    text("mouseRearOnModel=" + mousePoints._2, width/3, height - 65)
+    text("cameraView=" + cameraView, width/3, height - 50)
+    text("angles=" + angles, width/3, height - 35)
+    text("angleEventSourcing=" + angleEventSourcing, width/3, height - 20)
+
+  }
+
+  var angles = new Angles
+  var angleEventSourcing = new Angles
+  def applyKeyPressedAngles(): Unit = {
+    if (angleEventSourcing != Angles.origin) {
+      angles = angles.add(angleEventSourcing)
+    }
+  }
+
+  override def keyPressed: Unit = {
+    if (key == CODED) {
+      angleEventSourcing = keyCode match {
+        case UP => angleEventSourcing.addY(radians(0.5f))
+        case DOWN => angleEventSourcing.addY(radians(-0.5f))
+        case LEFT => angleEventSourcing.addX(radians(-0.5f))
+        case RIGHT => angleEventSourcing.addX(radians(0.5f))
+        case _ => angleEventSourcing // do nothing
+      }
     }
 
   }
+  override def keyReleased: Unit = {
+    if (key == CODED) angleEventSourcing = Angles.origin
+  }
+
 
 }
 object CameraViewApp {
