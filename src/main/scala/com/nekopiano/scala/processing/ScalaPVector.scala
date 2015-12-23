e21b72284a3d43b679275ef65cecbee5cacf2d55
La Musique <lamusique@users.noreply.github.com>
2015-12-24 05:35:17 +0900

    Support full coordination in ScalaPVector.

--- src/main/scala/com/nekopiano/scala/processing/ScalaPVector.scala
+++ src/main/scala/com/nekopiano/scala/processing/ScalaPVector.scala
@@ -18,10 +18,12 @@ case class ScalaPVector(var x:Float, var y:Float, var z:Float = 0) {
   // ================
   // Operation
   // ================
+  def set(settingX:Float=this.x, settingY:Float=this.y, settingZ:Float=this.z) = ScalaPVector(settingX, settingY, settingZ)
   def setX(settingX:Float) = ScalaPVector(settingX, this.y, this.z)
   def setY(settingY:Float) = ScalaPVector(this.x, settingY, this.z)
   def setZ(settingZ:Float) = ScalaPVector(this.x, this.y, settingZ)
 
+  def add(settingX:Float, settingY:Float, settingZ:Float) = ScalaPVector(this.x + settingX, this.y + settingY, this.z + settingZ)
   def addX(settingX:Float) = ScalaPVector(this.x + settingX, this.y, this.z)
   def addY(settingY:Float) = ScalaPVector(this.x, this.y + settingY, this.z)
   def addZ(settingZ:Float) = ScalaPVector(this.x, this.y, this.z + settingZ)
