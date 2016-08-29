1ee6352aba8835d96305aca1a9e55a8a049e4819
La Musique <lamusique@users.noreply.github.com>
2016-08-30 00:42:01 +0900

    Use Joda DateTime for CSV read on H2.

--- src/test/scala/com/nekopiano/scala/sandbox/spec/slick/CsvSlick.scala
+++ src/test/scala/com/nekopiano/scala/sandbox/spec/slick/CsvSlick.scala
@@ -14,7 +14,7 @@ import org.joda.time.format.DateTimeFormat
 object CsvSlick extends App {
 
 
-  Some(new PrintWriter("./sample-test.csv")).foreach{p => p.write("id,name,age,dob\n1,Alice,23,\"19950822\"\n2,Bob,34,\"19950411\"\n3,Chris,30,\"19960222\""); p.close}
+  Some(new PrintWriter("./sample-test.csv")).foreach{p => p.write("id,name,age,dob,update_date\n1,Alice,23,\"19950822\",\"1995/8/22\"\n2,Bob,34,\"19990411\",\"1999/4/11\"\n3,Chris,30,\"19960222\",\"1996/2/22\""); p.close}
 
 
   // the base query for the Users table
@@ -48,15 +48,23 @@ object CsvSlick extends App {
   } finally db.close
 }
 
-case class Row(id: Option[Int] = None, dob:Date, name: String, age:Short)
+//case class Row(id: Option[Int] = None, dob:Date, name: String, age:Short)
+case class Row(id: Option[Int] = None, dob:DateTime, updateDate:Date, name: String, age:Short)
 
 class Rows(tag: Tag) extends Table[Row](tag, "csvread('./sample-test.csv')") {
   // Auto Increment the id primary key column
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
 
-  implicit val utilDate2StringMapper = SystemDateMapper.utilDate2StringMapper
-  def dob = column[Date]("DOB")
+  implicit val dateTime2StringMapper = SystemDateMapper.dateTime2StringMapper
+  def dob = column[DateTime]("DOB")
+
+  //implicit val utilDate2StringMapper = SystemDateMapper.utilDate2StringMapper
+  //def dob = column[Date]("DOB")
+
   //def dob = column[Date]("DOB")(SystemDateMapper.utilDate2StringMapper)
+
+  implicit val utilDate2StringMapper = SystemDateMapper.utilDate2StringMapper
+  def updateDate = column[Date]("UPDATE_DATE")
 
   // The name can't be null
   def name = column[String]("NAME")
@@ -66,20 +74,21 @@ class Rows(tag: Tag) extends Table[Row](tag, "csvread('./sample-test.csv')") {
 
   import SystemDateMapper._
 
-  def * = (id.?, dob, name, age) <> (Row.tupled, Row.unapply)
+  def * = (id.?, dob, updateDate, name, age) <> (Row.tupled, Row.unapply)
 }
 
 
 
 object SystemDateMapper {
-  val dateFormatter = DateTimeFormat.forPattern("yyyyMMdd")
-
-  implicit val dateTime2StringMapper = MappedColumnType.base[DateTime, String](
-    { dateTime => dateFormatter.print(dateTime) },
-    { dateString => dateFormatter.parseDateTime(dateString) })
+  val dateFormatterSeparate = DateTimeFormat.forPattern("yyyy/M/d")
+  val dateFormatterDigits = DateTimeFormat.forPattern("yyyyMMdd")
 
   implicit val utilDate2StringMapper = MappedColumnType.base[Date, String](
-    { utilDate => dateFormatter.print(new DateTime(utilDate)) },
-    { dateString => dateFormatter.parseDateTime(dateString).toDate })
+    { utilDate => dateFormatterSeparate.print(new DateTime(utilDate)) },
+    { dateString => dateFormatterSeparate.parseDateTime(dateString).toDate })
+
+  implicit val dateTime2StringMapper = MappedColumnType.base[DateTime, String](
+    { dateTime => dateFormatterDigits.print(dateTime) },
+    { dateString => dateFormatterDigits.parseDateTime(dateString) })
 
 }
