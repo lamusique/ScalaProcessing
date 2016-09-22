e6273ca0b34824392b6a433b97449f1de245a23b
La Musique <lamusique@users.noreply.github.com>
2016-09-23 03:03:20 +0900

    - Fix lag issues caused by typo. - Add phases.

--- src/test/scala/com/nekopiano/scala/processing/sandbox/sound/harmonic/HarmonicFlows.scala
+++ src/test/scala/com/nekopiano/scala/processing/sandbox/sound/harmonic/HarmonicFlows.scala
@@ -72,14 +72,28 @@ object HarmonicFlows extends App {
 
     println("created toneActor=" + toneActor)
     val amp = 0.3 / number
-    toneActor ! ToneMessage(number, pitch, amp, System.currentTimeMillis())
+    val phase = Utility.random(0, 2 * math.Pi)
+
+    toneActor ! ToneMessage(number, pitch, phase, amp, System.currentTimeMillis())
     toneActor
   }}
 
 
 }
 
-case class ToneMessage(number:Int, freq:Double, amp:Double, currentTimeMillis:Long)
+object Utility {
+
+  val random = new Random()
+  def random(max:Double):Double = random.nextDouble() * max
+  def random(low: Double, high: Double): Double = {
+    if (low >= high) return low
+    val diff = high - low
+    random(diff) + low
+  }
+}
+
+
+case class ToneMessage(number:Int, freq:Double, phase:Double, amp:Double, currentTimeMillis:Long)
 case class ToneChangeMessage(number:Int, freq:Option[Double], amp:Option[Double], pan:Option[Double], freqLag:Option[Double], ampLag:Option[Double], panLag:Option[Double], currentTimeMillis:Long)
 class ToneActor extends Actor {
 
@@ -91,14 +105,6 @@ class ToneActor extends Actor {
 
   val system = context.system
   import system.dispatcher
-
-  val random = new Random()
-  def random(max:Double):Double = random.nextDouble() * max
-  def random(low: Double, high: Double): Double = {
-    if (low >= high) return low
-    val diff = high - low
-    random(diff) + low
-  }
 
   val synthPromise = Promise[Synth]
   lazy val synth:Synth = Await.result(synthPromise.future, 5 seconds)
@@ -122,35 +128,21 @@ class ToneActor extends Actor {
 
       //sender ! "Done"
 
-      changeTone()
+      println(synth.productIterator.mkString("\n"))
+
+      changeTone(ToneChangeMessage(number, Option(freq), Option(amp), Option(0), Option(0),Option(0),Option(0),System.currentTimeMillis()))
 
       sender ! this + " started."
     }
     case msg:ToneChangeMessage => {
 
-      msg.freqLag match {
-        case None => // do nothing
-        case Some(hertz) => changePitch(hertz)
-      }
-      msg.ampLag match {
-        case None => // do nothing
-        case Some(amp) => changeAmp(amp)
-      }
 
-      msg.freq match {
-        case None => // do nothing
-        case Some(hertz) => changePitch(hertz)
-      }
-      msg.amp match {
-        case None => // do nothing
-        case Some(amp) => changeAmp(amp)
-      }
-
-      changeTone()
+      changeTone(msg)
     }
     //case a:Any => "This may be an object proper. a=" + a
     case _ => println("This is unexpected.")
   }
+
   def play(freq:Double, amp:Double) {
     import de.sciss.synth._
     import Ops._
@@ -159,7 +151,53 @@ class ToneActor extends Actor {
     synthPromise.success(synthLocal)
   }
 
-  def changeTone() {
+  def changeTone(msg:ToneChangeMessage) {
+
+    msg.freq match {
+      case None => // do nothing
+      case Some(freq) => {
+        synth.set("freq" -> freq)
+        this.freq = freq
+      }
+    }
+    msg.freqLag match {
+      case None => // do nothing
+      case Some(freqLag) => {
+        synth.set("freqLag" -> freqLag)
+        this.freqLag = freqLag
+      }
+    }
+    msg.amp match {
+      case None => // do nothing
+      case Some(amp) => {
+        synth.set("amp" -> amp)
+        this.amp = amp
+      }
+    }
+    msg.ampLag match {
+      case None => // do nothing
+      case Some(ampLag) => {
+        synth.set("ampLag" -> ampLag)
+        this.ampLag = ampLag
+      }
+    }
+    msg.pan match {
+      case None => // do nothing
+      case Some(pan) => {
+        synth.set("pan" -> pan)
+        this.pan = pan
+      }
+    }
+    msg.panLag match {
+      case None => // do nothing
+      case Some(panLag) => {
+        synth.set("panLag" -> panLag)
+        this.panLag = panLag
+      }
+    }
+
+
+    import Utility._
 
     val delay = random(20)
 
@@ -167,36 +205,26 @@ class ToneActor extends Actor {
     amp = amp + ampDiff
     if (amp < 0 ) amp = 0
     if (amp > 0.2 ) amp = 0.2
-
-    ampLag = random(10)
+    ampLag = random(20)
 
     val freqDiff = random(-50, 50)
+    //val freqDiff = random(-200, 200)
     freq = freq + freqDiff
-
-    freqLag = random(10)
+    freqLag = random(20)
 
     pan = random(-1, 1)
-    panLag = random(10)
+    panLag = random(20)
 
-    //val change = ToneChangeMessage(number, None, Option(amp), System.currentTimeMillis())
-    //val change = ToneChangeMessage(number, Option(freq), Option(amp), None, None, System.currentTimeMillis())
-    //val change = ToneChangeMessage(number, Option(freq), Option(amp), Option(freqLag), Option(ampLag), System.currentTimeMillis())
+
+
+
     val change = ToneChangeMessage(number, Option(freq), Option(amp), Option(pan), Option(freqLag), Option(ampLag), Option(panLag), System.currentTimeMillis())
+    //val change = ToneChangeMessage(number, None, Option(amp), Option(pan), Option(freqLag), Option(ampLag), Option(panLag), System.currentTimeMillis())
 
     val cancellable = context.system.scheduler.scheduleOnce(delay seconds, self, change)
     println(self + " changes " + change + " after " + delay + " seconds.")
   }
 
-  def changePitch(freq:Double) {
-
-
-    synth.set("freq" -> freq)
-    this.freq = freq
-  }
-  def changeAmp(amp:Double) {
-    synth.set("amp" -> amp)
-    this.amp = amp
-  }
 }
 
 
@@ -216,9 +244,10 @@ object SuperColliderServer extends LazyLogging {
 
   val sineLagSynth = SynthDef("sineLag") {
     val freq = "freq".kr(261.6)
+    val freqlag = "freqLag".kr(1.0)
+    val phase = "phase".kr(0.0)
     val amp = "amp".kr(.2)
-    val freqlag = "freqlag".kr(1.0)
-    val amplag = "amplag".kr(1.0)
+    val amplag = "ampLag".kr(1.0)
     val pan = "pan".kr(0.0)
     val panLag = "panLag".kr(1.0)
 
@@ -226,7 +255,8 @@ object SuperColliderServer extends LazyLogging {
     val lagAmp = Lag.kr(amp, amplag)
     val lagPan = Lag.kr(pan, panLag)
 
-    val src = SinOsc.ar(lagFreq).madd(lagAmp, 0)
+    //val src = SinOsc.ar(lagFreq).madd(lagAmp, 0)
+    val src = SinOsc.ar(lagFreq, phase).madd(lagAmp, 0)
     val pan2 = Pan2.ar(src, lagPan)
     Out.ar(0, pan2)
   }
